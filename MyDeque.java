8e0e205325d36832d3acd0c847f83a93f6774996
Jason Zheng <zzheng3@stuy.edu>
2019-03-21 14:48:05 -0400

    Done with toString.

--- MyDeque.java
+++ MyDeque.java
@@ -3,18 +3,25 @@ public class MyDeque<E> {
   private int size, start, end;
   public MyDeque() {
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[10];
+    E[] d = (E[]) new Object[10];
     data = d;
   }
   public MyDeque(int initialCapacity) {
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[initialCapacity];
+    E[] d = (E[]) new Object[initialCapacity];
     data = d;
   }
   public int size() {
     return data.length;
   }
-  public String toString(){ }
+  public String toString() {
+    String display = "[";
+    for (int i = 0; i < data.length - 1; i = i + 1) {
+      display = display + data[i] + ", ";
+    }
+    display = display + data[length - 1] + "]";
+    return display;
+  }
   public void addFirst(E element){ }
   public void addLast(E element){ }
   public E removeFirst(){ }
