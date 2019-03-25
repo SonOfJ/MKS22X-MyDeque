3e6c1e372e410aa0c1181f1b0c98213c321cf09f
Jason Zheng <zzheng3@stuy.edu>
2019-03-25 00:26:11 -0400

    The driver finally compiles.

--- MyDeque.java
+++ MyDeque.java
@@ -1,14 +1,13 @@
 import java.util.*;
+@SuppressWarnings("unchecked")
 public class MyDeque<E> {
   private E[] data;
   private int size, start, end;
   public MyDeque() {
-    @SuppressWarnings("unchecked")
     E[] d = (E[]) new Object[10];
     data = d;
   }
   public MyDeque(int initialCapacity) {
-    @SuppressWarnings("unchecked")
     E[] d = (E[]) new Object[initialCapacity];
     data = d;
   }
