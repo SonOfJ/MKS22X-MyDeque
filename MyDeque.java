c8076d274d48cde75a73324a54134e070aef2db6
Jason Zheng <zzheng3@stuy.edu>
2019-03-24 23:49:25 -0400

    Updated resize.

--- MyDeque.java
+++ MyDeque.java
@@ -15,7 +15,7 @@ public class MyDeque<E> {
     return data.length;
   }
   private void resize() {
-    int[] bigger = new int[data.length + 1]; //Let's just assume resize increases the maximum size by 1.
+    int[] bigger = new int[data.length * 2 + 1]; //Let's just assume resize increases the maximum size by 1.
     for (int i = 0; i < data.length; i = i + 1) { //Copy over elements.
       bigger[i] = data[i];
     }
