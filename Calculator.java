e09dfa0e99abb34876cf0d0a34f22d3a26eb0d28
Jason Zheng <zzheng3@stuy.edu>
2019-03-31 20:24:02 -0400

    Decided to do a queue with double.

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,5 @@
 public class Calculator {
   public static double eval(String s) {
-    MyDeque<String> que = new MyDeque<String>(s.length());
     String[] expression = s.split(" "); //Split the string at the spaces.
-
-
+    MyDeque<Double> queue = new MyDeque<Double>(expression.length);
 }
