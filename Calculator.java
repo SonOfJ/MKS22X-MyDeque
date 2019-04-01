343ef75cbe447a6651fa3205ca8a45db95526446
Jason Zheng <zzheng3@stuy.edu>
2019-03-31 20:33:05 -0400

    My attempt at parsing doubles.

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,11 @@ public class Calculator {
   public static double eval(String s) {
     String[] expression = s.split(" "); //Split the string at the spaces.
     MyDeque<Double> queue = new MyDeque<Double>(expression.length);
+    for (int i = 0; i < expression.length; i = i + 1) {
+      try {
+        queue.addLast(Double.parseDouble(expression)); //Is the thing a number?
+      } catch (NumberFormatException e) { //If it's not...
+      //Leave it.
+      }
+    }
 }
