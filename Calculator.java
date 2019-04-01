70a181212c5d35e1bbee11c502f716ec3a2f4fde
Jason Zheng <zzheng3@stuy.edu>
2019-03-31 21:07:58 -0400

    Correct minor mistake in code.

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     MyDeque<Double> queue = new MyDeque<Double>(expression.length);
     for (int i = 0; i < expression.length; i = i + 1) {
       try {
-        queue.addLast(Double.parseDouble(expression)); //Is the thing a number?
+        queue.addLast(Double.parseDouble(expression[i])); //Is the thing a number?
       } catch (NumberFormatException e) { //If it's not...
       //Leave it.
       }
